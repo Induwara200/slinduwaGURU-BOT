893eb6eb1f2b01ff312ca4ee95c7b17a26558dc7
Induwara200 <146737807+Induwara200@users.noreply.github.com>
2023-10-03 19:48:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94789064604', 'me', true],
+  ['94789064604', 'sl max induwa', true], 
   [''] 
 ] //Number of owners
 
